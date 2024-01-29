5f4a72fa591122db1395eebdfeea2e234158dd29
2DYXBOSS <pirateavie225@gmail.com>
2024-01-29 22:33:05 +0000

    comm

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-import pywhatkit
+# import pywhatkit
 
 # Configurations pour le serveur SMTP
 
