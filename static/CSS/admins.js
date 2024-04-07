f8ba674e2595f9d4b3d27ea6ba53cb2c0d2aa07a
2DY XBOSS <140612341+2DYXBOSS@users.noreply.github.com>
2024-04-07 20:44:04 +0000

    commit

---
+++ static/CSS/admins.js
@@ -0,0 +1,53 @@
+
+
+
+function activatione(cliqueurid,activeid,imagew) {
+    et = "#" + cliqueurid + ""
+    const cliquere  = document.querySelector(et)
+    eteq = "#" + activeid + ""
+    let sortier  = document.querySelector(eteq)
+    eteqq = "#" + imagew + ""
+    let imahe  = document.querySelector(eteqq)
+
+    cliquere.addEventListener('click',()=>{
+        sortier.classList.add("active");
+    
+        imahe.src = `/static/IMAGE/bacj.png`
+    })
+
+}
+function effacfe(cliqueurid,activeid,imagew) {
+    et = "#" + cliqueurid + ""
+    const cliquere  = document.querySelector(et)
+    eteq = "#" + activeid + ""
+    let sortier  = document.querySelector(eteq)
+    eteqq = "#" + imagew + ""
+    let imahe  = document.querySelector(eteqq)
+
+    cliquere.addEventListener('click',()=>{
+        sortier.classList.remove("active");
+    
+        imahe.src = `/static/IMAGE/inf.png`
+    })
+
+}
+
+// function moinsp(ecranid) {
+//     couterw =document.querySelector("#couterw").value
+//     et = "#" + ecranid + ""
+//     let ecraid  = document.querySelector(et)
+//     if (ecraid.value >= 1){
+//         ecraid.value = parseInt(ecraid.value) - 1; 
+//         somiu = document.querySelector("#somiu")
+    
+//         somiu.innerHTML = parseInt(somiu.innerHTML)  - 1// Mettre à jour le contenu de l'élément
+//         quantiteplos = document.querySelector("#quantiteplos")
+//         quantiteplos.value = somiu.innerHTML
+//         lepris = document.querySelector("#lepris")
+//         lepris.innerHTML = lepris.innerHTML - parseInt(couterw)
+//     }
+    
+//     console.log(et);
+    
+//     return 0
+// }
